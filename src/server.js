9a8dd5c486e9ca0e074953f3a69f2b701dbce96c
LeTuanKiet29102002 <letuankiet02042k2@gmail.com>
2023-09-28 13:12:56 +0700

    install-cors

--- src/server.js
+++ src/server.js
@@ -3,12 +3,13 @@ import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine";
 import initWebRoutes from "./route/web";
 import connectDB from "./config/connectDB";
+import cors from 'cors';
 require("dotenv").config();
 
 // let dotenv = require("dotenv");
 
 let app = express();
-
+app.use(cors({ credentials: true, origin: true }));
 //config app
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
